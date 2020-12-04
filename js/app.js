ef5b1c3581431cf055388bfc120db7f8db504a05
Ryne Laster <ryne.laster@gmail.com>
2020-12-04 14:05:34 -0600

    Adding node modules and changes to some JS files.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,13 @@
+/*
 console.log('whats good hubspot');
+
+const express = require('express');
+const axios = require('axios');
+
+const http = require('http');
+const hubspot = require('@hubspot/api-client');
+
+const app = express();
+
+app.listen(3000, () => console.log('listening on http://localhost:3000'));
+*/
--- js/node.js
+++ js/node.js
@@ -1,6 +1,8 @@
-const http = require('http');
 
+
+/*
 http.createServer(function (req, res) {
 	res.writeHead(200, {'Content-Type': 'text/html'});
 	res.end('Hello Hubspot!');
 }).listen(8080);
+*/
